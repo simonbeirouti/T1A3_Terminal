e30c3d1dda766ac5ac70bbb34d2c3107a6aacc1a
simonbeirouti <hello@simonbeirouti.com>
2022-07-15 00:41:45 +1000

    create bash script to create, check and start game

---
+++ run_first.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Run the cards.py to generate the cards.
+python3 cards.py
+
+# Run the error_checker.py to check for initial errors.
+python3 error_checker.py
+
+# Run the main program to play the game.
+# python3 main.py
+
+
