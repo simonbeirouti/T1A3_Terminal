ecd72c734917e3fa4df4244782149f6793b23c98
simonbeirouti <hello@simonbeirouti.com>
2022-07-15 02:25:50 +1000

    add result table into functions with write feature

--- ext_functions.py
+++ ext_functions.py
@@ -1,4 +1,5 @@
 from rich.console import Console
+from rich.table import Table
 console = Console()
 
 def ask_wager():
@@ -12,3 +13,17 @@ def ask_wager():
     else:
         console.print('You entered an invalid wager. Defaulting to a wager of 2.')
     return wager
+
+def results_table(player, community, opponent):
+    table = Table(title="Flips for lolz")
+
+    table.add_column("Players Cards", justify="left", style="cyan")
+    table.add_column("Board", justify="center", style="magenta")
+    table.add_column("Opponents Cards", justify="right", style="cyan")
+
+    table.add_row(f"{player}", f"{community}", f"{opponent}")
+    with open('results.txt', 'a') as f:
+        f.write(f"\nPlayers cards: {player}")
+        f.write(f"\nOpponents cards: {opponent}")
+        f.write(f"\nCommunity cards:{community}\n")
+    console.print(table)
